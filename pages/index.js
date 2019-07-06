f8da45232ce8eac63657d2768215667d98a0a694
Joshua Raichur <raichur@utexas.edu>
2019-07-06 13:49:00 -0500

    List and Instant Search

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,72 @@
-import Layout from '../components/Layout'
-import Link from 'next/link'
-import fetch from 'isomorphic-unfetch'
+import React from "react";
+import Layout from "../components/Layout";
+import Link from "next/link";
+import fetch from "isomorphic-unfetch";
 
-const Index = props => (
-    <Layout>
-        <ul>
-            {props.shows.map(show => (
-                <li key={show.id}>
-                    <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
-                        <a>{show.name}</a>
-                    </Link>
-                </li>
-            ))}
-        </ul>
-    </Layout>
-)
+class Index extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      shows: [],
+      value: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.getShows = this.getShows.bind(this)
+  }
 
-Index.getInitialProps = async function() {
-    const res = await fetch(`http://api.tvmaze.com/search/shows?q=mr+robot`)
-    const data = await res.json()
+  getShows() {
+    fetch(`http://api.tvmaze.com/search/shows?q=${this.state.value}`)
+    .then(results => {
+      return results.json();
+    })
+    .then(data => {
+      let shows = data.map(result => (
+        <li key={result.show.id}>
+          <Link
+            as={`/p/${result.show.id}`}
+            href={`/post?id=${result.show.id}`}
+          >
+            <a>{result.show.name}</a>
+          </Link>
+        </li>
+      ));
+      this.setState({ shows: shows });
+    });
+  }
 
-    console.log(`Data fetched. Show count: ${data.length}`)
 
-    return {
-        shows: data.map(entry => entry.show)
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+    if(event.target.value.length > 4) {
+        this.getShows()
     }
+  }
+
+  handleSubmit(event) {
+    this.getShows()
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <Layout>
+        <form className="form" id="addItemForm" onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            className="input"
+            autoComplete="off"
+            id="addInput"
+            placeholder="Search for TV shows"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+          <button className="button is-info">Search</button>
+        </form>
+        {this.state.shows}
+      </Layout>
+    );
+  }
 }
 
-export default Index
+export default Index;
--- pages/post.js
+++ pages/post.js
@@ -1,52 +1,51 @@
-import Layout from '../components/Layout.js'
-import fetch from 'isomorphic-unfetch'
-import React from 'react'
+import Layout from "../components/Layout.js";
+import fetch from "isomorphic-unfetch";
+import React from "react";
 
 class Post extends React.Component {
+  static async getInitialProps(context) {
+    const { id } = context.query;
+    const res = await fetch(`http://api.tvmaze.com/shows/${id}/episodes`);
+    const episodes = await res.json();
 
-    static async getInitialProps(context) {
-        const { id } = context.query
-        const res = await fetch(`http://api.tvmaze.com/shows/${id}/episodes`)
-        const episodes = await res.json()
-    
-        console.log(`Fetched shows count: ${episodes.length}`)
+    var total_runtime_minutes = 0;
+    var total_runtime_hours = { hours: 0, minutes: 0 };
 
-        var total_runtime_minutes = 0
-        var total_runtime_hours = { hours: 0, minutes: 0}
+    episodes.map(episode => {
+      total_runtime_minutes += episode.runtime;
+    });
 
-        episodes.map((episode) => {
-            total_runtime_minutes += episode.runtime
-        })
+    // Convert minutes to hours
+    total_runtime_hours.hours = Math.floor(total_runtime_minutes / 60);
+    total_runtime_hours.minutes = total_runtime_minutes % 60;
 
-        total_runtime_hours.hours = Math.floor(total_runtime_minutes / 60)
-        total_runtime_hours.minutes = total_runtime_minutes % 60
+    return { episodes, total_runtime_hours };
+  }
 
-        return { episodes, total_runtime_hours }
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    constructor(props) {
-        super(props)
-    }
-
-    render() {
-        return (
-            <Layout>
-                <h1>{this.props.total_runtime_hours.hours} hours {this.props.total_runtime_hours.minutes} minutes total</h1>
-                {this.props.episodes.map((episode) => {
-                    return (
-                        <li key={episode.id}>
-                            <h4>{episode.name}</h4>
-                            <p>{episode.runtime} minutes</p>
-                            <p>{episode.summary.replace(/<[/]?p>/g, '')}</p>
-                            <img src={episode.image.medium} />
-                        </li>
-                    )
-                })}
-            </Layout>
-        )
-    }
+  render() {
+    return (
+      <Layout>
+        <h1>
+          {this.props.total_runtime_hours.hours} hours{" "}
+          {this.props.total_runtime_hours.minutes} minutes total
+        </h1>
+        {this.props.episodes.map(episode => {
+          return (
+            <li key={episode.id}>
+              <h4>{episode.name}</h4>
+              <p>{episode.runtime} minutes</p>
+              <p>{episode.summary.replace(/<[/]?p>/g, "")}</p>
+              <img src={episode.image.medium} />
+            </li>
+          );
+        })}
+      </Layout>
+    );
+  }
 }
 
-
-
-export default Post
+export default Post;
